b92ea973df9549dd089635ae68a1a9a4daebbb65
Tudor Dragan <tudorgk@gmail.com>
2018-08-26 08:05:11 +0200

    Added first nullability demo

---
+++ 1-nullability/src/Nullability.kt
@@ -0,0 +1,39 @@
+import kotlin.concurrent.thread
+
+var a: String? = null
+
+fun changeForever() {
+    thread {
+        while (true) {
+            randomVariableSetter()
+        }
+    }
+}
+
+@Synchronized
+fun randomVariableSetter() {
+    a = if (listOf(true, false).shuffled().first()) {
+        "test"
+    } else {
+        null
+    }
+}
+
+@Synchronized
+fun variableChecker() {
+    if (a != null) {
+        println("Variable a has value $a\n")
+    } else {
+        println("NULL!\n")
+    }
+}
+
+fun betterVariableChecker() {
+    a?.also {
+        println("Variable a has value $it\n")
+    } ?: println("NULL!\n")
+}
+
+
+
+
