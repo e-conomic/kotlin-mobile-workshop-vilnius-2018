de8fe84e22983358e4a962e245cb1c0bc2fc82a8
Tudor Dragan <tudorgk@gmail.com>
2018-08-29 10:04:40 +0200

    Added clone example.

--- 2-data-classes/src/DataClasses.kt
+++ 2-data-classes/src/DataClasses.kt
@@ -1,13 +1,19 @@
-open class Base {
+import org.apache.commons.lang3.SerializationUtils
+import java.io.Serializable
+
+open class Base : Serializable {
     var baseParam: String? = null
 }
 
-data class A(val name: String) : Base()
+data class A(val name: String) : Base(), Serializable
 
 val test1 = A("test1").apply { baseParam = "base1" }
 val test2 = test1.copy(name = "test2")
+val test3 = SerializationUtils.clone(test1)
 
 fun print(instance: A) {
     println(instance.toString() + "\n")
     println(instance.baseParam.toString() + "\n")
 }
+
+
