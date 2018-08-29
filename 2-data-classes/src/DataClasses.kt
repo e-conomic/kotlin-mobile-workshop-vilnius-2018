b93c7feaef613db7905f25d6c3483837099a44b6
Tudor Dragan <tudorgk@gmail.com>
2018-08-29 14:36:54 +0200

    Added examples for standard functions.

--- 2-data-classes/src/DataClasses.kt
+++ 2-data-classes/src/DataClasses.kt
@@ -5,11 +5,18 @@ open class Base : Serializable {
     var baseParam: String? = null
 }
 
-data class A(val name: String) : Base(), Serializable
+data class A(var name: String) : Base(), Serializable
+
+data class B(var aClass: A)
+
 
 val test1 = A("test1").apply { baseParam = "base1" }
 val test2 = test1.copy(name = "test2")
 val test3 = SerializationUtils.clone(test1)
+
+
+var test4 = B(test1)
+val test5 = test4.copy()
 
 fun print(instance: A) {
     println(instance.toString() + "\n")
---
+++ 3-standard/src/Standard.kt
@@ -0,0 +1,88 @@
+import org.jetbrains.annotations.Mutable
+
+/**
+ * Created by tudorgk on 29/08/2018.
+ * Copyright (C) 2018 Visma e-conomic A/S
+ */
+
+object Standard {
+    fun testLet() {
+        val str: String = "string"
+
+        val result = str.let {
+            println("Receiver: $this\n")
+            println("Argument: $it\n")
+
+            13
+        }
+        print("Result: $result\n")
+    }
+
+    fun testAlso() {
+        val str: String = "string"
+
+        val result = str.also {
+            println("Receiver: $this\n")
+            println("Argument: $it\n")
+
+            13
+        }
+        print("Result: $result\n")
+    }
+
+    fun testApply() {
+        val list: MutableList<String> = mutableListOf("A", "B", "C")
+
+        val result = list.apply {
+            println("Receiver: $this\n")
+
+            add("D")
+            13
+        }
+        print("Result: $result\n")
+        print("Original: $list\n")
+        print("Same object?: ${result === list}")
+    }
+
+    fun testRun1() {
+        val str: String = "string"
+
+        val result = run {
+            println("Receiver: $this\n")
+
+            13
+        }
+        print("Result: $result\n")
+    }
+
+    fun testRun2() {
+        val str: String = "string"
+
+        val result = str.run {
+            println("Receiver: $this\n")
+
+            13
+        }
+        print("Result: $result\n")
+    }
+
+    fun testWith() {
+
+        data class DemoDataClass(var param1: Int = 1, var param2: String = "demo")
+
+        val demo = DemoDataClass()
+
+        val result = with(demo) {
+            println("Receiver: $this\n")
+            println("Param1: $param1\n")
+            println("Param2: $param2\n")
+
+            param1 = 2
+            param2 = "test"
+
+            13
+        }
+        print("Result: $result\n")
+        print("Original: $demo\n")
+    }
+}
